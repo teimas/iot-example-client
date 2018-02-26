682cc1b81150560545641b33efafa71f54cdda11
Iago Elizechea <iago.elizechea@teimas.com>
2018-02-26 12:44:08 +0100

    Set up speedometer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,42 @@ import React, { Component } from 'react';
 import ReactSpeedometer from "react-d3-speedometer";
 
 class App extends Component {
+  state = {
+    value: 50
+  };
+
+  tick() {
+    fetch("/value").then(response => {
+      response.text().then(s => {
+        var n = parseInt(s);
+        if (this.state.value !== n) {
+          this.setState({ value: n });
+        }
+      });
+    });
+  }
+
+  getInitialState() {
+    return { value: 0 };
+  }
+
+  componentDidMount() {
+    this.tick();
+    this.timer = setInterval(this.tick.bind(this), 100);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
   render() {
     return (
       <div className="App">
-
-        <ReactSpeedometer />
-
+        <ReactSpeedometer
+          maxValue={100}
+          minValue={0}
+          value={this.state.value}
+        />
       </div>
     );
   }
